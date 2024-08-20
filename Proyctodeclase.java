e3eba7c911171cf9f549c7ed1f772bc68a994a61
Exhr2004 <96797415+Exhr2004@users.noreply.github.com>
2024-08-19 22:08:18 -0600

    Add files via upload

---
+++ Proyctodeclase.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyctodeclase;
+import java.util.Scanner;
+/**
+ *
+ * @author xavi
+ */
+public class Proyctodeclase {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+         int k = 5;
+        
+        System.out.println("seleccione la operacion");
+        System.out.println("1. quiero cotizar un viaje");
+        System.out.println("2. quiero solicitar un viaje");
+        System.out.println("seleccione la opcion");
+        int opcion = entrada.nextInt();
+        
+        System.out.println("ingrese la fecha");
+        float fecha = entrada.nextFloat();
+        System.out.println("ingrese los quilometros a recorrer");
+        int km = entrada.nextInt();
+        
+        int resultado = 0;
+        
+        switch (opcion){
+            case 1:
+              
+                resultado = Multiplicar(km,k);
+                System.out.println("el precio del viaje es" + resultado);
+        
+        }
+        
+        System.out.println("ingrese el tipo de unidad que desea");
+        System.out.println("1. veinte pasajeros");
+        System.out.println("2. treinta pasajeros");
+        System.out.println("3. cuarenta pasajeros");
+        System.out.println("4. cincuenta pasajeros");
+        int opcio = entrada.nextInt();
+        
+        
+    }
+    public static int Multiplicar(int km, int k){
+        return km * k;
+    }
+}
